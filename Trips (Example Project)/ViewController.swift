fd52214fc49fe70de6394b94e4289ddd8b1bff6d
Keenan Hauber <keenan@hauber-family.net>
2021-04-30 13:59:11 +1000

    Initial Commit

---
+++ Trips (Example Project)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trips (Example Project)
+//
+//  Created by Keenan Hauber on 30/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
